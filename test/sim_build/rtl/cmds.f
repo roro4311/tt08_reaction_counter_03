4fb6ed0bcc3efdc545b8df227529eeb5b153d2eb
roro4311 <41744527+roro4311@users.noreply.github.com>
2024-08-01 15:49:34 -0400

    Update cmds.f

--- test/sim_build/rtl/cmds.f
+++ test/sim_build/rtl/cmds.f
@@ -1,1 +1,8 @@
 +timescale+1ns/1ps
+# List of Verilog source files for simulation
+/home/runner/work/tt08_reaction_counter_03/tt08_reaction_counter_03/src/tt_um_reaction_timer.v
+/home/runner/work/tt08_reaction_counter_03/tt08_reaction_counter_03/src/reaction_timer.v
+/home/runner/work/tt08_reaction_counter_03/tt08_reaction_counter_03/src/spi_driver.v
+/home/runner/work/tt08_reaction_counter_03/tt08_reaction_counter_03/test/tb.v
+/home/runner/pdk/sky130A/libs.ref/sky130_fd_sc_hd/verilog/primitives.v
+/home/runner/pdk/sky130A/libs.ref/sky130_fd_sc_hd/verilog/sky130_fd_sc_hd.v
