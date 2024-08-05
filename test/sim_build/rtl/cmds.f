f731b636be984e85b628d8730abee7c55fe2fd81
roro4311 <41744527+roro4311@users.noreply.github.com>
2024-08-05 09:21:31 -0400

    Update cmds.f

--- test/sim_build/rtl/cmds.f
+++ test/sim_build/rtl/cmds.f
@@ -1,4 +1,10 @@
 # Include the top-level module and any necessary Verilog files
+/home/runner/pdk/sky130A/libs.ref/sky130_fd_sc_hd/verilog/primitives.v
+/home/runner/pdk/sky130A/libs.ref/sky130_fd_sc_hd/verilog/sky130_fd_sc_hd.v
+/home/runner/work/tt08_reaction_counter_03/tt08_reaction_counter_03/test/gate_level_netlist.v
+/home/runner/work/tt08_reaction_counter_03/tt08_reaction_counter_03/test/reaction_timer.v
+/home/runner/work/tt08_reaction_counter_03/tt08_reaction_counter_03/src/spi_driver.v
+/home/runner/work/tt08_reaction_counter_03/tt08_reaction_counter_03/src/tt_um_reaction_timer.v
 $(PWD)/src/tt_um_reaction_timer.v
 $(PWD)/src/reaction_timer.v
 $(PWD)/src/spi_driver.v
